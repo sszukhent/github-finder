0ef01c441f6134e7fa4ab2387d36c44dba8034d7
sszukhent <commandershepard989@gmail.com>
2020-02-17 14:13:26 -0500

    added useritem component

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class UserItem extends Component {
+  state = {
+    id: 'id',
+    avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+    login: 'mojombo',
+    html_url: 'https://github.com/mojombo'
+  };
+
+  render() {
+    const { avatar_url, login, html_url } = this.state;
+
+    return (
+      <div className='card text-center'>
+        <img
+          src={avatar_url}
+          alt=''
+          className='round-img'
+          style={{ width: '60px' }}
+        />
+        <h3>{login}</h3>
+        <div>
+          <a href={html_url} className='btn btn-dark btn-sm my-1'>
+            More
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default UserItem;
